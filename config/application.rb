ba659235590e9b29fa6b7952fb9bdd2216e68bb5
やす <i.20010911@icloud.com>
2023-05-13 20:45:20 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module HerokuDeploy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.logger = Logger.new(STDOUT)
 
     # Configuration for the application, engines, and railties goes here.
     #
